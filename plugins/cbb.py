5a234c24cfc5dac51312f96abe9a587b3597bbc2
HGAnimeTest <153988055+HGAnimeTest@users.noreply.github.com>
2024-03-14 19:06:57 +0530

    Add files via upload

---
+++ plugins/cbb.py
@@ -0,0 +1,28 @@
+#(©)Codexbotz
+
+from pyrogram import __version__
+from bot import Bot
+from config import OWNER_ID
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+
+@Bot.on_callback_query()
+async def cb_handler(client: Bot, query: CallbackQuery):
+    data = query.data
+    if data == "about":
+        await query.message.edit_text(
+            text = f"<b>1. First Join the channel \n2. Tap on Original link again or Reload ⚡️ \n3. Tap on Start and Done ✅.\nFor more help Join <a href=https://t.me/Alliance_Weebs>Support Group</a></b>",
+            disable_web_page_preview = True,
+            reply_markup = InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("🍁 ᴄʟᴏꜱᴇ", callback_data = "close")
+                    ]
+                ]
+            )
+        )
+    elif data == "close":
+        await query.message.delete()
+        try:
+            await query.message.reply_to_message.delete()
+        except:
+            pass
